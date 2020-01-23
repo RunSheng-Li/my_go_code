855a2fea3e3bd780845021ed4208bc74ab7455a1
sam <sam@samdeMacBook-Air.local>
2020-01-23 21:44:59 +0800

    区别静态类型和动态类型C

---
+++ go语言入门经典/理解类型/区分静态类型和动态类型.go
@@ -0,0 +1,38 @@
+//所谓强类型语言，指的是错误使用了类型时，编译器将引发错误
+//所谓动态类型语言，指的是为了执行程序，运行时会将一种类型转换为另一种类型，或者编译器没有实现类型系统
+
+// 类型简介
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func sayHello(s string) string {
+//	return "Hello " + s
+//}
+//
+//func main() {
+//	fmt.Println(sayHello("Sam"))
+//}
+
+//go语言中的类型
+package main
+
+import (
+	"fmt"
+)
+
+func addition(x int, y int) int {
+	return x + y
+
+}
+
+func main() {
+	fmt.Println(addition(1, 4))
+
+	//传递类型不正确的参数 下面这段代码会报错
+	//var s string ="fuck"
+	//fmt.Println(addition(1, s))
+
+}
