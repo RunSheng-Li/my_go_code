fea1c58894e7b1512e1ab10702534fa1cff86470
sam <sam@samdeMacBook-Air.local>
2020-01-23 22:43:03 +0800

    使用布尔类型C

---
+++ go语言入门经典/理解类型/使用布尔类型.go
@@ -0,0 +1,31 @@
+//布尔值只能为true或false
+
+
+//声明布尔变量
+//package main
+//
+//import (
+//	"fmt"
+//)
+//
+//func main(){
+//	var b bool
+//	//如果没有给布尔变量赋值，它将默认为false
+//	fmt.Println(b)
+//
+//}
+
+//给布尔变量重新赋值
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var b bool
+	fmt.Println(b)
+	b = true
+	fmt.Println(b)
+
+}
