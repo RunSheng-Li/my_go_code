f6b94e418eac08bf90f7cda468125132d31c8e86
sam <li-runsheng@outlook.com>
2020-01-25 19:26:01 +0800

    指针的应用

---
+++ 我的demo/指针的应用.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var p *int = &a
+
+	a = 100
+	fmt.Println("a = ", a)
+
+	*p = 250 // 借助a变量的地址，操作a对应的空间
+	fmt.Println("a = ", a)
+	fmt.Println("*p = ", *p)
+
+	a = 1000
+	fmt.Println("*p = ", *p)
+
+}
+
+//指针就是地址。指针变量就是存储地址的变量
+//*p 解引用 间接引用
