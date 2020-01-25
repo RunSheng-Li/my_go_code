52e130d21b0f6dd66f5aed8673da1d0fd94d09fa
sam <li-runsheng@outlook.com>
2020-01-25 18:52:42 +0800

    理解变量作用域

---
+++ go语言入门经典/理解变量/理解变量作用域.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a = "hello world"
+
+func main() {
+	fmt.Printf("%v\n", a)
+	b := true
+	if b {
+		fmt.Printf("%v\n", b)
+		i := 42
+		if b != false {
+			fmt.Printf("%v\n", i)
+		}
+
+	}
+
+}
