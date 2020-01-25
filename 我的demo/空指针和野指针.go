66c168525c39969c46a3fcc6486a2d45f4251de4
sam <li-runsheng@outlook.com>
2020-01-25 22:29:34 +0800

    空指针野指针，还有补充了一些内容

---
+++ 我的demo/空指针和野指针.go
@@ -0,0 +1,24 @@
+//空指针：未被初始化的指针。   var p *int     *p--->error
+
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//
+//	var p *int
+//	fmt.Println(*p)
+//
+//}
+
+//野指针：被一片无效的地址空间初始化。
+
+package main
+
+import "fmt"
+
+func main(){
+	var p *int = 0
+	fmt.Println(*p)
+
+}
