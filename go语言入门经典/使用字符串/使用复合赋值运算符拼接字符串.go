16a50fbe6f6ae3db2ccbb22424088ad00ec0163c
sam <li-runsheng@outlook.com>
2020-01-28 10:43:19 +0800

    字符串拼接

---
+++ go语言入门经典/使用字符串/使用复合赋值运算符拼接字符串.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	s := "heihei"
+	s += "\nhehe"
+	fmt.Println(s)
+
+}
---
+++ go语言入门经典/使用字符串/将多个字符串拼接成一个.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	s := "你好" + "世界"
+	fmt.Println(s)
+
+}
