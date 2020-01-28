42b506b0d83e65d718dc3a88e15b412993291b90
sam <li-runsheng@outlook.com>
2020-01-28 19:45:07 +0800

    定义不定参函数

---
+++ go语言入门经典/使用函数/定义不定参数函数.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sumNumbers(numbers ...int) int {
+
+	total := 0
+
+	for _, number := range numbers {
+		total += number
+
+	}
+	return total
+
+}
+
+func main() {
+
+	result := sumNumbers(1, 2, 3, 4)
+	fmt.Printf("结果是%v\n", result)
+
+}
