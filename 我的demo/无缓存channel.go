61d8a139fbe212c8df87f514b4159211e303e0f8
sam <li-runsheng@outlook.com>
2020-01-29 16:25:20 +0800

    无缓存channel

---
+++ 我的demo/无缓存channel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println("子go程,i=", i)
+			ch <- i
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		num := <-ch
+		fmt.Println("主go程读", num)
+
+	}
+
+}
