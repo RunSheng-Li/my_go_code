b73720e83abc723998b0dfb70e80f734dc50231d
sam <li-runsheng@outlook.com>
2020-01-29 21:06:47 +0800

    select

---
+++ go语言入门经典/通道简介/select语句.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func ping1(c chan string) {
+	time.Sleep(time.Second * 1)
+	c <- "ping on channel1"
+}
+
+func ping2(c chan string) {
+	time.Sleep(time.Second * 2)
+	c <- "ping on channel2"
+
+}
+
+func main() {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go ping1(channel1)
+	go ping2(channel2)
+
+	select {
+	case msg1 := <-channel1:
+		fmt.Println("收到", msg1)
+
+	case msg2 := <-channel2:
+		fmt.Println("收到", msg2)
+
+	}
+
+}
