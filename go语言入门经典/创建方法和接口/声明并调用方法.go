046f280debeebe77c810642b0be963fa582e4b35
sam <li-runsheng@outlook.com>
2020-02-07 19:21:37 +0800

    声明并调用方法

--- go语言入门经典/创建方法和接口/声明并调用方法.go
+++ go语言入门经典/创建方法和接口/声明并调用方法.go
@@ -1,1 +1,28 @@
-package 创建方法和接口
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Movie struct {
+	Name   string
+	Rating float64
+}
+
+func (m *Movie) summary() string {
+
+	var r = strconv.FormatFloat(m.Rating, 'f', 1, 64)
+	return m.Name + ", " + r
+
+}
+
+func main() {
+	var m = Movie{
+		Name:   "Spiderman",
+		Rating: 3.2,
+	}
+
+	fmt.Println(m.summary())
+
+}
