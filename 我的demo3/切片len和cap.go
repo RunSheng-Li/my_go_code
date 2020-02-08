f5a9dd4cfa6c2ed0ef9cb08a45fe2653d06f2f59
sam <li-runsheng@outlook.com>
2020-02-08 17:14:02 +0800

    增加例子

--- 我的demo3/切片len和cap.go
+++ 我的demo3/切片len和cap.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+//func main() {
+//
+//	s := []int{}
+//	for i := 0; i < 10; i++{
+//		s = append(s, i)
+//		fmt.Println(len(s), cap(s))
+//	}
+//
+//}
 
-	s := []int{}
-	for i := 0; i < 10; i++{
-		s = append(s, i)
-		fmt.Println(len(s), cap(s))
-	}
+func main(){
+
+	year := []string{"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"}
+	Q2 := year[3:6]
+	fmt.Println(Q2,len(Q2),cap(Q2))
+	summer := year[5:8]
+	fmt.Println(summer,len(summer),cap(summer))
+	summer[0]="Unknow"
+	fmt.Println(Q2)
+	fmt.Println(year)
+
 
 }
